4e44c7f6e6894a022bc35b876fdeb138416b2a0e
nadinaaa <43186448+nadinaaa@users.noreply.github.com>
2018-09-28 11:26:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,54 +1,56 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-let m=currency;
- if(m<=0)
- {
-    var p = {};
-    return p;
- }
-if(m>10000)
-{
-    var p={
-        error: "You are rich, my friend! We don't have so much coins for exchange"
+    let m=currency;
+     if(m<=0)
+     {
+        var k = {};
+        return k;
+     }
+    if(m>10000)
+    {
+        var k={
+            error: "You are rich, my friend! We don\'t have so much coins for exchange"
+        };
+        return k;
+    }
+    let h=0,q=0,d=0,n=0,p=0;
+    let i=0;
+    let mas=[50,25,10,5,1];
+    do
+    {
+        m-=mas[i];
+        if(m<0)
+        {
+            m+=mas[i];
+            i++;
+        }
+        else
+        {
+            if(i===0) h+=1;
+            if(i===1) q+=1;
+            if(i===2) d+=1;
+            if(i===3) n+=1;
+            if(i===4) p+=1;
+        }
+    }
+    while(m!=0);
+    
+    var obj = {
+        H:0,
+        Q:0,
+        D:0,
+        N:0,
+        P:0
     };
-    return p;
+    obj.H=h;
+    obj.Q=q;
+    obj.D=d;
+    obj.N=n;
+    obj.P=p;
+    if(h==0) delete obj.H;
+    if(q==0) delete obj.Q;
+    if(d==0) delete obj.D;
+    if(n==0) delete obj.N;
+    if(p==0) delete obj.P;
+    return obj;
 }
-var obj = {
-    H:0,
-    Q:0,
-    D:0,
-    N:0,
-    P:0
-};
-
-while(m>0)
-{
-    m-=50;
-    obj.H+=1;
-}
-if(m===0) return obj;
-m+=50;
-let i=0;
-let mas=[25,10,5,1];
-do{
-m-=mas[i];
-if(m<0)
-{
-    m+=mas[i];
-    i++;
-}
-else
-{
-    if(i===0)
-    obj.Q+=1;
-    if(i===1)
-    obj.D+=1;
-    if(i===2)
-    obj.N+=1;
-    if(i===3)
-    obj.P+=1;
-}
-}while(m!=0);
-return obj;
-}
-
+    
